9cd437f68527822eb80386834c3e9705ab983ffb
KiboOst <KiboOst@users.noreply.github.com>
2017-03-16 08:25:56 +0100

    endpoint example to start actions from IFTTT

---
+++ IFTTTactions.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+
+Action endpoint for IFTTT buttons or applets
+
+call:
+http://www.mydomain.com/path/to/action.php?action=turnlighton
+
+you can also pass other params with action.php?action=turnlighton&camera=camera1
+
+*/
+
+//get url variable:
+if(isset($_GET['action']))
+{
+	require($_SERVER['DOCUMENT_ROOT']."/path/to/NetatmoPresenceAPI.php");
+	$_Presence = new NetatmoPresenceAPI($Netatmo_user, $Netatmo_pass);
+    $action = $_GET['action'];
+    $cam = 'my camera';
+    $param = 0;
+    if(isset($_GET['camera'])) $cam = $_GET['camera'];
+    if(isset($_GET['param'])) $param = $_GET['param'];
+
+    if ($action == "turnlighton") $_Presence->setFloodlight($cam, "on");
+    if ($action == "turnlightoff") $_Presence->setFloodlight($cam, "off");
+    if ($action == "alerthumanon") $_Presence->setHumanAlert(2);
+    if ($action == "monitoringoff") $_Presence->setMonitoring($cam, "off");
+    if ($action == "monitoringon") $_Presence->setMonitoring($cam, "on");
+    //http://www.mydomain.com/path/to/action.php?action=setintensity&camera=cam2&param=100
+    if ($action == "setintensity") $_Presence->setIntensity($cam, $param);
+    //etc.
+
+}
+
+?>
